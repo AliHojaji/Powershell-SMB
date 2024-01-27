57f021bd0da746fa405325af02560243a6ed9a55
Ali Hojaji <140146049+AliHojaji@users.noreply.github.com>
2024-01-27 13:24:31 +0330

    Add files via upload
    Encrypting SMB Shares with Powershell

---
+++ Encrypting-SMB- Shares.ps1
@@ -0,0 +1,15 @@
+﻿#--- Author : Ali Hojaji ---#
+
+#--*-------------------------*--#
+#---> Encrypting-SMB-Shares <---#
+#--*-------------------------*--#
+
+# SMB 3.1.1 with backward compatibility
+# SMB 3.0.2 with backward compatibility
+# SMB 3.0 with backward compatibility
+# SMB 2.1 with backward compatibility
+
+# Existing share
+Set-SmbServer Configuration -EncryptData $true
+Create a new SMB file share and enable encryption
+New-SmbShare -Name <sharename> -Path <pathname> - EncryptData $true
