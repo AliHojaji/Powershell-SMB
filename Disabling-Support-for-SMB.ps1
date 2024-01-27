55bd75ea01f9772ffbbfa5f50d0eb1d531af5327
Ali Hojaji <140146049+AliHojaji@users.noreply.github.com>
2024-01-27 13:25:15 +0330

    Add files via upload
    Disabling Support for SMB with Powershell

---
+++ Disabling-Support-for-SMB.ps1
@@ -0,0 +1,9 @@
+﻿#--- Author : Ali Hojaji ---#
+
+#--*------------------------*--#
+#---> Disabling-SMB-Shares <---#
+#--*------------------------*--#
+
+#Vista through Server 2008 R2 require a require a registry edit
+#Windows 8 and Server 2012 or later use
+set-SmbServerConfiguration -EnableSMB1Protocol $false
